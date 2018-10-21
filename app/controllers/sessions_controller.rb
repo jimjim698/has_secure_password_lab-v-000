9a37f51be301f737f4abb127a5667c76c9a19b95
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-21 02:09:00 +0000

    automatically backed up by learn

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  
+  def new 
+  end 
+  
+  def create 
+    
+    user = User.find_by(name: params[:username])
+    return head(:forbidden) unless @user.authenticate(params[:password]) 
+    session[:user_id] = user.id
+    redirect_to users_welcome_path
+  end 
+  
+  
+  
+
+  
+end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,17 @@
+class UsersController < ApplicationController
+  
+  
+  def new 
+  end 
+  
+  def create 
+    user = User.new(name: params[:username], password: params[:password])
+     user.save 
+     session[:user_id] = user.id 
+     redirect_to users_welcome_path
+  end 
+  
+  def welcome
+    @user = User.find(session[:user_id])
+  end 
+end
