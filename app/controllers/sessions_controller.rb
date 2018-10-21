4d4f5840c80984df514a8805a6aabc992b91c719
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-21 02:17:37 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     
     @user = User.find_by(name: params[:username])
     return head(:forbidden) unless @user.authenticate(params[:password]) 
-    session[:user_id] = user.id
+    session[:user_id] = @user.id
     redirect_to users_welcome_path
   end 
   
