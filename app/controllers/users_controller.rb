7abd8d7cc11f24b9821ff8b83c76a782ce74ab9f
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-21 02:26:01 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,6 @@ class UsersController < ApplicationController
   
   def create 
     user = User.new(name: params[:user][:name], password: params[:user][:password])
-    binding.pry 
      user.save 
      session[:user_id] = user.id 
      redirect_to users_welcome_path
