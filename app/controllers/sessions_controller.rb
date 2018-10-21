d51f2d8a03a317a6a2fac9b90edb22b05a4392d6
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-21 02:22:42 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,6 @@ class SessionsController < ApplicationController
   def create 
     
     @user = User.find_by(name: params[:user][:name])
-    binding.pry
     return head(:forbidden) unless @user.authenticate(params[:user][:password]) 
     session[:user_id] = @user.id
     redirect_to users_welcome_path
