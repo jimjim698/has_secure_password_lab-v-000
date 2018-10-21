1bbe887c1497d4f2dd4fa7e99eec31b32f565f27
James Lotruglio <jimlotruglio@yahoo.com>
2018-10-21 02:30:34 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,13 @@ class UsersController < ApplicationController
   end 
   
   def create 
+    if params[:user][:password] == params[:user][:password_confirmation]
     user = User.new(name: params[:user][:name], password: params[:user][:password])
      user.save 
      session[:user_id] = user.id 
      redirect_to users_welcome_path
+   else 
+     redirect_to users_new_path
   end 
   
   def welcome
